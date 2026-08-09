198f64557d87e0b0bf86e0093e1f6c531306c7db
puroome <129413484+puroome@users.noreply.github.com>
2026-08-09 23:45:10 +0900

    Add files via upload

--- js/core.js
+++ js/core.js
@@ -8,8 +8,8 @@ const MAP_PROJECTION = Object.freeze({
   originLatitude: 36,
   originLongitude: 127.5,
   scale: 8230,
-  offsetX: 335.22,
-  offsetY: 384.23
+  offsetX: 335.3,
+  offsetY: 384.3
 });
 const SVG_RING_CACHE = new WeakMap();
 
